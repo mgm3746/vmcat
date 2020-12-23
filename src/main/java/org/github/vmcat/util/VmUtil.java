15e84bf7a0dcdc02ef09480f55bd11854f2fda4a
mmillson <mgm101@gmail.com>
2020-12-23 12:37:47 -0500

    Don't version control Eclipse config files.

--- src/main/java/org/github/vmcat/util/VmUtil.java
+++ src/main/java/org/github/vmcat/util/VmUtil.java
@@ -86,7 +86,7 @@ public class VmUtil {
      * @return The value for the given property file and key.
      */
     public static final String getPropertyValue(String propertyFile, String key) {
-        ResourceBundle rb = ResourceBundle.getBundle("META-INF" + System.getProperty("file.separator") + propertyFile);
+        ResourceBundle rb = ResourceBundle.getBundle("META-INF." + propertyFile);
         return rb.getString(key);
     }
 
